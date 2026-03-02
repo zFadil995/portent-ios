b27f07b3a820fe50dbab79ee77ba475150ebd4c1
Fadil Žilić <zfadil995@gmail.com>
2026-03-02 15:46:46 +0100

    chore: initial ios project

---
+++ portent/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  portent
+//
+//  Created by Fadil Žilić on 2. 3. 2026..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ portent/portentApp.swift
@@ -0,0 +1,17 @@
+//
+//  portentApp.swift
+//  portent
+//
+//  Created by Fadil Žilić on 2. 3. 2026..
+//
+
+import SwiftUI
+
+@main
+struct portentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
