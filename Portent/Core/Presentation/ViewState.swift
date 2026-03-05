fd9cd31fd3aa54810a279529d28172a8e1f2c3d3
Fadil Žilić <zfadil995@gmail.com>
2026-03-05 15:51:35 +0100

    refactor: remove legacy ContentView and ViewState

--- Portent/Core/Presentation/ViewState.swift
+++
@@ -1,7 +0,0 @@
-/// Legacy loading-state enum. Use `UiState` for all new screens.
-enum ViewState<T> {
-    case idle
-    case loading
-    case success(T)
-    case error(title: String? = nil, message: String?)
-}
